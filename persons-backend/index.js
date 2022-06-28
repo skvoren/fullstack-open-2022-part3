2ae1f34b05b50c9a3c92632a0c548c11ca3deb62
skvoren <skvoren@outlook.com>
2022-06-28 23:36:36 +0400

    exercise 3.13-3.14

--- persons-backend/index.js
+++ persons-backend/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const fs = require("fs")
-const path = require("path")
-const cors = require("cors")
+const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -12,35 +12,10 @@ app.use(express.urlencoded())
 morgan.token('post-data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :post-data'))
 
-
-
-const PORT = 3001
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "phone": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "phone": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "phone": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "phone": "39-23-6423122"
-    }
-]
+const PORT = process.env.PORT
 
 const countOfPersons = () => {
-    return persons.length;
+    return Person.count
 }
 
 const getDate = () => {
@@ -53,17 +28,20 @@ const infoData = {
     Date: getDate()
 }
 
+const isPersonExist = (name, phone) => {
+    return Person.find({name: name, phone: phone}).count() === 1
+}
+
 app.get('/api/persons', (_request, response) => {
-    response.json(persons)
+    Person.find({}).then(result => {
+        response.json(result)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const personData = persons.find(person => person.id === id)
-
-    if (personData) {
-        response.json(personData)
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 app.get('/api/info', (_request, response) => {
@@ -71,40 +49,32 @@ app.get('/api/info', (_request, response) => {
 })
 
 app.post('/api/create-person', (request, response) => {
-
     const body = request.body
 
     if (!body.name || !body.phone){
         response.status(400).json({
             error: "empty body of request!"
         })
-    } else if (persons.find(p => p.name === body.name || p.phone === body.phone)){
+    } else if (isPersonExist(body.name, body.phone)){
         response.status(400).json({
             error: "person added already"
         })
     } else {
-        const newPerson = {
+        const person = new Person({
             name: body.name,
             phone: body.phone,
-            id: Date.now()
-        }
+        })
 
-        persons = persons.concat(newPerson)
-
-        response.json(persons)
+        person.save().then(result => {
+            response.json(result)
+        })
     }
-
-
-
-
-
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(persons => persons.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    })
 })
 
 app.listen(PORT, () => {
